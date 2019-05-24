5ac52ec18b7768b2203bd3566ae068f3c068d38e
latalavera <49410289+latalavera@users.noreply.github.com>
2019-05-24 15:17:59 +0200

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,21 +1,24 @@
-import json
-
+imero creamos varios diccionarios con las posibles variantes de cada caracteristica
 hair_color = {"black" : "CCAGCAATCGC", "brown" : "GCCAGTGCCG", "blonde" : "TTAGCTATCGC"}
 facial_shape = {"square" : "GCCACGG", "round" : "ACCACAA", "oval" : "AGGCCTCA"}
 eye_color = {"blue" : "TTGTGGTGGC", "green" : "GGGAGGTGGC", "brown" : "AAGTAGTGAC"}
 gender = {"female" : "TGAAGGACCTTC", "male" : "TGCAGGAACTTC"}
 race = {"white" : "AAAACCTCA", "black" : "CGACTACAG", "asian" : "CGCGGGCCG"}
 
+#creamos 4 listas para cada uno de los sospechosos con sus caracteristicas
 eva = ["female", "white", "blonde", "blue", "oval"]
 larisa = ["female", "white", "brown", "brown", "oval"]
 mantej = ["male", "white", "black", "blue", "oval"]
 miha = ["male", "white", "brown", "green", "square"]
 
+#abrimos el archivo dna.txt en modo lectura y guardamos su valor en la variable dna
 with open("dna.txt" , "r") as dna_file:
     dna = dna_file.read()
-
+#creamos una nueva lista llamada caracteristicas
 caracteristicas = []
 
+#hacemos un bucle que recorra la lista de cada uno de los sospechosos y vaya añadiendo a la lista caracteristicas
+#los valors que concidan
 for x in eva:
     if x == "female":
         caracteristicas.append("TGAAGGACCTTC")
@@ -45,8 +48,10 @@ for x in eva:
         caracteristicas.append("ACCACAA")
     elif x == "oval":
         caracteristicas.append("AGGCCTCA")
-
+#creamos una variable nueva en la que todos los valores que tiene caracteristicas sean concatenados
 dna_eva = str(caracteristicas[0])+str(caracteristicas[1])+str(caracteristicas[2]+str(caracteristicas[3])+str(caracteristicas[4]))
+# comparamos si la concatenacion de la variable de eva esta dentro de la variable dna y en caso de ser afirmativo imprima un
+# mensaje diciendo que es el culpable
 if dna_eva in dna:
     print("el culpable es eva")
 
@@ -147,9 +152,12 @@ for x in miha:
         caracteristicas.append("ACCACAA")
     elif x == "oval":
         caracteristicas.append("AGGCCTCA")
+
 dna_miha = str(caracteristicas[0])+str(caracteristicas[1])+str(caracteristicas[2]+str(caracteristicas[3])+str(caracteristicas[4]))
-print (dna_miha)
-print(dna)
 if dna_miha in dna:
-    print("el culpable es miha")
+    print("el culpable es mantej")
+# otra manera de comprobar si la variable dna_miha esta incluida en la variable dna
+if dna.find(dna_miha) >= 0:
+	print("el culpable es miha")
+
 
